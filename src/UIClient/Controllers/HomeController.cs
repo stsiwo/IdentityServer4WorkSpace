4c3397cb87748d91a858ef5eb9d38f2628ce9f7f
Satoshi Iwao <stsiwo@gmail.com>
2019-05-23 12:19:39 -0700

    added sign out action method

--- src/UIClient/Controllers/HomeController.cs
+++ src/UIClient/Controllers/HomeController.cs
@@ -36,6 +36,11 @@ namespace UIClient.Controllers
             return View();
         }
 
+        public IActionResult Logout()
+        {
+            return SignOut("Cookies", "oidc");
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
